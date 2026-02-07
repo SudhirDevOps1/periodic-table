a57f00d632e6ca1c46f1e774ea75715489b95be8
Sudhir Singh <sudhirk926235@gmail.com>
2026-02-07 13:04:42 +0530

    v1

---
+++ server.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+import webbrowser
+import os
+
+PORT = 8000
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    webbrowser.open(f"http://localhost:{PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    print("Server stopped.")
